68e49f94da1a20bce59d79c9a719ea612b91bc11
Jose Daniel Mancilla Mancilla <josedmancillam@gmail.com>
2016-09-03 21:44:19 -0400

    [MOD] layout

--- siscond_owner/controllers/controllers_owners.py
+++ siscond_owner/controllers/controllers_owners.py
@@ -8,8 +8,12 @@ from openerp.http import request
 
 class SiscondOwners(http.Controller):
 
-	@http.route('/siscond/owners/massivedownloadformat', type='http', auth="public", website=True)
+	@http.route('/hola', type='http', auth="public", website=True)
    	def massivedownloadformat(self, **kw):
+   		print ',mnmnjnkjnsdjnskjdnkjsndkjsnd'
+   		print ',mnmnjnkjnsdjnskjdnkjsndkjsnd'
+   		print ',mnmnjnkjnsdjnskjdnkjsndkjsnd'
+   		print ',mnmnjnkjnsdjnskjdnkjsndkjsnd'
 		templateId='siscond_owner.massive_download_format_owners'
 		values={
     		'hola':'hola'	
