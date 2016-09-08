5ba9865877e34478de87776c0a75f71968e47e5b
Jose Daniel Mancilla Mancilla <josedmancillam@gmail.com>
2016-09-08 18:34:49 -0400

    [ADD] New Select

--- siscond_owner/static/src/js/controllers/siscond_owners.js
+++ siscond_owner/static/src/js/controllers/siscond_owners.js
@@ -1,8 +1,10 @@
 angular
     .module('MyfirstApp')
-    .controller('SelectedTextController', function($scope) {
+    .controller('SelectedTextController', function($timeout,$scope) {
       $scope.items = [1, 2, 3, 4, 5, 6, 7];
       $scope.selectedItem;
+      $scope.user = null;
+      $scope.users = null;
       $scope.getSelectedText = function() {
         if ($scope.selectedItem !== undefined) {
           return "You have selected: Item " + $scope.selectedItem;
@@ -10,4 +12,21 @@ angular
           return "Please select an item";
         }
       };
+
+
+      $scope.loadUsers = function() {
+
+    // Use timeout to simulate a 650ms request.
+    return $timeout(function() {
+
+      $scope.users =  $scope.users  || [
+        { id: 1, name: 'Scooby Doo' },
+        { id: 2, name: 'Shaggy Rodgers' },
+        { id: 3, name: 'Fred Jones' },
+        { id: 4, name: 'Daphne Blake' },
+        { id: 5, name: 'Velma Dinkley' }
+      ];
+
+    }, 650);
+  };
     }); 
