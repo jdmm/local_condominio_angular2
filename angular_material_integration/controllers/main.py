ee6a900a2fad47f4e4dddf758abd4d1b6f6a6a50
Jose Daniel Mancilla Mancilla <josedmancillam@gmail.com>
2016-09-10 12:17:05 -0400

    [ADD] angular - javascript modules system overview integration, firsh test

--- angular_material_integration/controllers/main.py
+++ angular_material_integration/controllers/main.py
@@ -10,13 +10,13 @@ QueryURL=main_blog.QueryURL
 class index(main.Home):
     @http.route('/', auth='public', website=True)
     def index(self):
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-    	print 'ndjsnkjdnksjbdksbdkhsbdkhsdbk'
-        velocidad=60
+        return http.request.website.render('angular_material_integration.layout_angular_material', {'edit':False
+        })
+
+
+    @http.route('/name_search/select', auth='public', website=True)
+    def name_search(self,**post):
+
+        
         return http.request.website.render('angular_material_integration.layout_angular_material', {'edit':False
         })
--- siscond_owner/static/src/js/controllers/siscond_owners.js
+++ siscond_owner/static/src/js/controllers/siscond_owners.js
@@ -1,3 +1,11 @@
+odoo.define('siscond_owner.orm', function (require) {
+"use strict";
+var core = require('web.core');
+var base = require('web_editor.base');
+var website = require('website.website');
+var Model = require('web.Model');
+var users = new Model('res.users');
+console.log(users);
 angular
     .module('MyfirstApp')
     .controller('SelectedTextController', function($timeout,$scope) {
@@ -19,14 +27,34 @@ angular
     // Use timeout to simulate a 650ms request.
     return $timeout(function() {
 
-      $scope.users =  $scope.users  || [
-        { id: 1, name: 'Scooby Doo' },
-        { id: 2, name: 'Shaggy Rodgers' },
-        { id: 3, name: 'Fred Jones' },
-        { id: 4, name: 'Daphne Blake' },
-        { id: 5, name: 'Velma Dinkley' }
-      ];
+      $scope.users = $scope.users  || [];
+      console.log($scope.users.length)
+      if (!$scope.users.length){
+          users.call('name_search',[],
+                  {context:base.get_context()}).then(function (result) {
+          console.log(result);
+          $scope.users = $scope.users  || [];
+          for(var i=0;i<result.length;i++) {
+           $scope.users.push({ id: result[i][0], name: result[i][1] });
+          }
+          console.log( $scope.users)
+          // $scope.users =  $scope.users  || [
+          //   { id: 1, name: 'Scooby Doo' },
+          //   { id: 2, name: 'Shaggy Rodgers' },
+          //   { id: 3, name: 'Fred Jones' },
+          //   { id: 4, name: 'Daphne Blake' },
+          //   { id: 5, name: 'Velma Dinkley' }
+          // ];
+          console.log( $scope.users)
+        // do something with change_password result
+        });
 
-    }, 650);
+      }
+      
+    });
   };
-    }); 
+    });
+   
+});
+
+  
