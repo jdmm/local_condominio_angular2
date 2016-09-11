efb0a8cdcc92ee60b1ec36c673485b0bc2a137fd
Jonathan <jon.rey8995@gmail.com>
2016-09-11 12:35:44 -0400

    [add] file icons

--- siscond_owner/static/src/js/controllers/siscond_owners.js
+++ siscond_owner/static/src/js/controllers/siscond_owners.js
@@ -8,11 +8,11 @@ var towerObj = new Model('siscond_infrastructure.tower');
 
 angular
     .module('MyfirstApp')
-    .controller('SelectedTowers', function($timeout,$scope) {
+    .controller('SelectedTowers', function($timeout,$scope,$mdDialog) {
 	$scope.tower=undefined;
 	$scope.towers=null;
       $scope.loadTowers = function() {
-    return $timeout(function() {
+		return $timeout(function() {
       $scope.towers = $scope.towers  || [];
       if (!$scope.towers.length){
           towerObj.call('search_towers',['',[1,2,3,4]],
@@ -22,50 +22,40 @@ angular
 			$scope.towers.push({ id: result[i][0], name: result[i][1] });
           }
         });
-
-      }
-      
-      
-      
-      
-      
-
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
+		}
+	});
+	};
+	
+	$scope.getSelectedText = function() {
+		if ($scope.tower !== undefined) {
+		  return "Usted ha selecionado: Torre " + $scope.tower.name;
+		}else {
+		  return "Torres";
+		}}
+		
+	
+	$scope.dowloadExcel = function(ev) {
+		if ($scope.tower !== undefined){
+			var towerId=$scope.tower.id
+			  console.log(towerId)
+			  console.log(towerId)
+			  console.log(99999)
+			  console.log(99999)
+			}else{
+				$mdDialog.show(
+				  $mdDialog.alert()
+					.clickOutsideToClose(true)
+					.title('Aviso!')
+					.textContent('Seleccione una Torre.')
+					.ariaLabel('Alert Dialog Demo')
+					.ok('Cerrar')
+					.targetEvent(ev)
+				);
+				}
+    };
+		
+		
     });
-  };
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-      $scope.getSelectedText = function() {
-        if ($scope.tower !== undefined) {
-          return "Usted ha selecionado: Torre " + $scope.tower.name;
-        }else {
-          return "Torres";
-        }}
-  
-  
-  
-  
-  
-  
-    });
-   
 });
 
   
